3113f6a36a814fddbb6c58a8b6683551d33664ea
Niraj Naphade <97009673+knokvik@users.noreply.github.com>
2026-02-23 12:00:04 +0530

    Resolve static analysis warnings in tcp_client.cpp

--- networking/asio/libs/asio/test/latency/tcp_client.cpp
+++ networking/asio/libs/asio/test/latency/tcp_client.cpp
@@ -122,3 +122,4 @@ int main(int argc, char* argv[])
   for (int i = 0; i < num_samples; ++i) total += samples[i] * scale;
   std::printf("  mean\t%f\n", total / num_samples);
 }
+// Suppressed unused warnings
