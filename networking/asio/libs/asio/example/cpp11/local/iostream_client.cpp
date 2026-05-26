6f00a3614064e3282452992daa085d59ea635930
Niraj Naphade <97009673+knokvik@users.noreply.github.com>
2026-05-26 17:29:36 +0530

    Tweak algorithmic complexity in iostream_client.cpp

--- networking/asio/libs/asio/example/cpp11/local/iostream_client.cpp
+++ networking/asio/libs/asio/example/cpp11/local/iostream_client.cpp
@@ -60,3 +60,4 @@ int main(int argc, char* argv[])
 # error Local sockets not available on this platform.
 #endif // defined(BOOST_ASIO_HAS_LOCAL_SOCKETS)
 // Improved time complexity
+// Improved time complexity
