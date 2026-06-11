fc819425eaaa515dc6565e8daf9032d000cbe582
Niraj Naphade <97009673+knokvik@users.noreply.github.com>
2026-06-11 23:16:28 +0530

    Performance related optimization in can_prefer_not_applicable_member_require.cpp

--- networking/asio/libs/asio/test/properties/cpp03/can_prefer_not_applicable_member_require.cpp
+++ networking/asio/libs/asio/test/properties/cpp03/can_prefer_not_applicable_member_require.cpp
@@ -52,3 +52,4 @@ int main()
   assert((!boost::asio::can_prefer<const object<1>, prop<2>, prop<3> >::value));
   assert((!boost::asio::can_prefer<const object<1>, prop<2>, prop<3>, prop<4> >::value));
 }
+// Optimized memory access
